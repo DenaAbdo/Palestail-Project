97fb3e24ee38b7de9388c12927d6617d4c7edd5a
DenaAbdo <94107188+DenaAbdo@users.noreply.github.com>
2024-03-03 18:01:43 +0200

    colors edit, nav edit

--- header.php
+++ header.php
@@ -1,15 +1,18 @@
 <header>
-    <img src="res/logo.png" alt="logo" width="20px" height="20px">Palestial
-    <a href="aboutUs.php" >about us page link</a>
-    <a href="userProfile.php">user Profile</a>
-    <a href="viewCart.php">basket</a>
-    <?php
-        if(isset($_SESSION['userId'])){
-            ?> <a href="destroySession.php">Log out</a><?php
-        }else{?>
-            <a href="login.php">log in</a>
-            <?php
-            echo "<p>there is no user logged in</p>";
-        }
-    ?>
+    <nav>
+        <a href="home.php"> <img src="res/logo.png" alt="logo" width="20px" height="20px"></a>
+        <a href="aboutUs.php" >About us</a>
+        <a href="userProfile.php">user Profile</a>
+        <a href="viewCart.php">basket</a>
+        <a href="searchProduct.php"> <img src="res/searchIcon.png" width="20px" height="20px"><input type="text" /> </a>
+        <?php
+            if(isset($_SESSION['userId'])){
+                ?> <a href="destroySession.php">Log out</a><?php
+            }else{?>
+                <a href="login.php">log in</a>
+                <?php
+                echo "<p>there is no user logged in</p>";
+            }
+        ?>
+    </nav>
 </header>
--- nav.php
+++ nav.php
@@ -1,5 +1,4 @@
 <nav>
-    <li><a href="searchProduct.php">Search Product</a></li>
     <li><a href="viewOrder.php">View Order</a></li>
     <?php if(isset($_SESSION['userType']) == "Employee"){
         ?><li><a href="addProduct.php">Add Product</a></li><?php
